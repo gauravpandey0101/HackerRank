4c3c8ede70cf08cf888d049590fba71b3d25328b
Gaurav Pandey <107850615+gauravpandey0101@users.noreply.github.com>
2022-06-25 23:28:25 +0530

    Add files via upload

---
+++ C/Hello World.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() 
+{
+	
+    char s1[100];
+    printf("Hello, World!\n");
+    fgets(s1,sizeof(s1),stdin);
+    puts(s1);
+  	
+    return 0;
+}
