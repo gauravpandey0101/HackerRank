120a103716cdf652665a6d7a3d75e19b05fa219d
Gaurav Pandey <107850615+gauravpandey0101@users.noreply.github.com>
2022-08-08 21:22:59 +0530

    Add files via upload

---
+++ C/structure_using_function.c
@@ -0,0 +1,28 @@
+/* 
+    ->  Here , we can only write simple program of "structure using pointer"
+    ->  struct keyword is used for define structure
+    ->  we created a structure type pointer variable to store a structure type variable address - ( struct pointer *ptr )
+*/
+
+#include<stdio.h>
+void show(struct a);              // Function prototype
+
+struct pointer
+{ 
+    int i;                        // Created a structure
+};
+
+int main()
+{   
+    struct pointer p;
+    struct pointer *ptr;
+    ptr = &p;
+    ptr->i = 1;                   // or we can also write this : (*ptr).i = 1;
+    show(p);                      // function calling
+    return 0;
+}
+
+void show(struct a)               // function definition
+{
+    printf("I = %d",a.i);
+}
