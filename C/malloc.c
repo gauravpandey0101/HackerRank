7c8e7473ddac4c56d151d8f38c56c2bdda5231e1
Gaurav Pandey <107850615+gauravpandey0101@users.noreply.github.com>
2022-08-10 00:31:25 +0530

    Add files via upload

---
+++ C/malloc.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int *ptr;
+    // allocate memory for pointers In C
+    ptr = (int *)malloc(5*sizeof(int));
+
+    for(int i = 0; i < 5; i++)
+    {
+    //  calculate number of square for each variables and print them  
+        ptr[i] = (i+1)*(i+1);
+        printf("%d = %d\n ", i+1,ptr[i]);
+    }
+    //  free memory allocated using malloc
+    free(ptr);
+    return 0;
+}
